1d8f1e3f86c2524a15d7d24709379c50910bb7fd
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2023-03-22 23:28:16 -0300

    add fonts & theme

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,9 +1,18 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
+    :root {
+        font-size: 62.5%;
+    }
+
     * {
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+    }
+
+    body {
+        font-weight: 500;
+        font-size: 1.6rem;
     }
 `
--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,4 +1,14 @@
 export const defaultTheme = {
   black: '#000000',
+  'gray-100': '#FBFAFF',
+  'gray-200': '#E3E3E3',
+  'gray-300': '#C7C7CD',
+  'gray-500': '#7C7C8A',
+  'gray-700': '#505059',
+  'brown-500': '#756966',
+  'brown-700': '#403937',
+  'purple-100': '#E2D6FF',
+  'purple-300': '#CAB3FF',
+  'purple-500': '#7C3AED',
   white: '#FFFFFF',
 }
