781af463ee31cfe0e6530d222b8ccf9edcfeec4b
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2023-03-22 23:03:54 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { ThemeProvider } from 'styled-components'
+
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
+import { Home } from './pages/Home'
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Home />
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,4 @@
+export const defaultTheme = {
+  black: '#000000',
+  white: '#FFFFFF',
+}
